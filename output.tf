602cc115db431827a4529b7fb31c991de8472826
abhinandanmishra31 <90231928+abhinandanmishra31@users.noreply.github.com>
2022-03-09 21:45:51 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "cidr" {
-    value = aws_vpc.aws-vpc.cidr_block  
+    value = aws_vpc.terraform-vpc.cidr_block  
 }
 output "vpc-id" {
-  value = aws_vpc.aws-vpc.id
+  value = aws_vpc.terraform-vpc.id
 }
 output "vpc-tendancy" {
-  value = aws_vpc.aws-vpc.instance_tenancy
+  value = aws_vpc.terraform-vpc.instance_tenancy
 }
