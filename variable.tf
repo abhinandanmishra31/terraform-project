f3a1e820cbc7f3855973158eccb35157ab3605d0
abhinandanmishra31 <90231928+abhinandanmishra31@users.noreply.github.com>
2022-03-09 21:15:33 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,12 +4,7 @@ variable "vpc" {
 variable "subnet" {
     type= list
 default =["172.30.0.0/24","172.30.16.0/24","172.30.64.0/24"]  
-}
-variable "tags" {
-    type=list
-    default = ["uber-vpc","user-pub-sub","uber-private-sub","uber-igw","user-pub-rt"
-    ,"uber-nat","uber-private-rt","user-webserver"]
-}
+    
 variable "key" {
     type = string
     default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDabLZSiLmAn53KxbPEldkMQqCOpNjwr9aXZ7JqcaE8Sn4S+9925RKW63EEpPtbw2nqMLU9jj6XAYG6/Od12oHLhP5F6xXwoI5q379U6KT7ppWgS5gzcL7dUu9UFrBZhUC+8HE/tiBDfRqUZ/G/vERGBapR3Ep4/ZvwR3kxajCi/36hWN5euBunKWlVN5DQ4+DVOLyu+nwzfpvhweei+YE5BgafjtiNcHT8hE33RNro+SawD5rnbUfrBRhemcSyv0Z97jVP+IlxL5qopfzp8y4c4Ie2HVpuph7KUuvlEBWPrJNpJ/jxNV6c1dhtr67qyycf4qJe1kgER0OVVyrY6MDMS8FMUPSaqPk54u3/uOu7Vfy8zirENE8GZfQ/VT06b7dogF9Z9YSvWMyCM2zIwFcEWqoN0MHXnreyksepRONu1puOddzc5qvhPJRVsIPqXaOC0wqZdMJd/0QEsz5FM8zfqR/WR8Q1FhWrgbssxgQ7mHNTpG5e1wCbrn0+A9IO6Z8= abhin@abhinandan-pc"
